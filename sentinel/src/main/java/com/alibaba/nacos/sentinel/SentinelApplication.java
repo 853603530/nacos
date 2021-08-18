25a727caaf23a49a6ea6110eb76a25f916f599de
原子中的宇宙 <853603530@qq.com>
2021-08-18 16:29:44 +0800

    Initial commit

---
+++ sentinel/src/main/java/com/alibaba/nacos/sentinel/SentinelApplication.java
@@ -0,0 +1,27 @@
+package com.alibaba.nacos.sentinel;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+    @RestController
+    public class TestController {
+
+        @GetMapping(value = "/hello")
+        @SentinelResource("hello")
+        public String hello() {
+            return "Hello Sentinel";
+        }
+
+    }
+}
